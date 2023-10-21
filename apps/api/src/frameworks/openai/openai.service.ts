380778d1ebe2913a8a27f727fce04d103e58956d
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-10-21 17:45:19 +0700

    refactor: fix duplicate code

--- apps/api/src/frameworks/openai/openai.service.ts
+++ apps/api/src/frameworks/openai/openai.service.ts
@@ -17,7 +17,7 @@ export class OpenaiService {
         messages: [{ role: 'user', content: userContent }],
         model: 'gpt-3.5-turbo',
       });
-      return chatCompletion;
+      return chatCompletion.choices[0].message.content;
     } catch (error) {
       throw new Error('Failed to create chat completion: ' + error.message);
     }
--- apps/api/src/gateway/events.gateway.ts
+++ apps/api/src/gateway/events.gateway.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { OpenaiService } from '../frameworks';
+import { ChatHistoryService } from '../usecase';
+import { Body } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
@@ -13,16 +15,33 @@ import { OpenaiService } from '../frameworks';
   },
 })
 export class EventsGateway {
-  constructor(private readonly openaiService: OpenaiService) {}
+  constructor(
+    private readonly openaiService: OpenaiService,
+    private readonly chatHistoryService: ChatHistoryService
+  ) {}
 
   @WebSocketServer()
   server: Server;
 
   @SubscribeMessage('events')
-  async handleEvent(@MessageBody() data: string) {
+  async handleEvent(
+    @MessageBody() data: string,
+    @Body('user_id') user_id: string,
+    @Body('topic_id') topic_id: string[]
+  ) {
     const response = await this.openaiService.createChatCompletion(data);
     setTimeout(() => {
       this.server.emit('events', response);
     }, 1000);
+
+    const chatData = {
+      message: data,
+      date: new Date(),
+      user_id: user_id,
+      topic_id: topic_id,
+    };
+
+    this.chatHistoryService.addChatHistory(chatData);
+    this.chatHistoryService.addChatHistory({ ...chatData, message: response });
   }
 }
