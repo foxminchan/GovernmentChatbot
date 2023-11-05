fd5b4e24a4413216eed5ce1eada6e985c8371f7e
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-11-05 19:05:08 +0700

    feat: add left toggle

--- apps/website/src/components/Navbar/index.tsx
+++ apps/website/src/components/Navbar/index.tsx
@@ -14,7 +14,7 @@ const navItems = [
   { name: 'Chatbot', link: '/chat', currrent: false, width: 'w-28' },
   {
     name: 'Hỗ trợ',
-    link: '/support',
+    link: '/policy',
     currrent: false,
     subMenu: true,
     width: 'w-24',
@@ -24,6 +24,11 @@ const navSupport = [
   { name: 'Điều khoản sử dụng', link: '/policy', currrent: false },
   { name: 'Hướng dẫn sử dụng', link: '/guide', currrent: false },
   { name: 'Thông báo', link: '/notification', currrent: false },
+];
+
+const navToggleMenu = [
+  { name: 'Đăng nhập', link: '/sign-in', currrent: false },
+  { name: 'Đăng ký', link: '/sign-up', currrent: false },
 ];
 
 export default function Navbar() {
@@ -52,6 +57,9 @@ export default function Navbar() {
 
   return (
     <nav className="w-full h-11 bg-white-smoke-100 left-44">
+      {menuVisible && (
+        <div className="absolute top-0 left-0 w-full h-full bg-[#00000033] md:hidden"></div>
+      )}
       <div className="relative flex w-auto h-full left-44">
         <div className="w-12 h-full bg-japonica-500">
           <Link
@@ -112,7 +120,14 @@ export default function Navbar() {
         </div>
       </div>
       {menuVisible && (
-        <div className="relative md:hidden bg-white-smoke-100">
+        <div
+          className={clsx(
+            'md:hidden bg-white-smoke-100 top-0 left-0 px-[15px] pt-2 absolute z-10',
+            menuVisible
+              ? 'h-full w-[260px] transition-all duration-[0.3s]'
+              : 'w-0 transition-all duration-[0.3s]'
+          )}
+        >
           <ul className="flex flex-col items-center">
             {navItems.map((item) => (
               <li
@@ -129,6 +144,15 @@ export default function Navbar() {
               </li>
             ))}
           </ul>
+          {navToggleMenu.map((item) => (
+            <Link
+              to={item.link}
+              key={item.name}
+              className="flex items-center justify-center w-full h-12 my-2 border rounded border-japonica-500 hover:bg-japonica-500 hover:text-white"
+            >
+              <span className="text-xl font-medium">{item.name}</span>
+            </Link>
+          ))}
         </div>
       )}
     </nav>
