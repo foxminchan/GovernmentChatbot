8d28bdd24b55e5bd048da5140ece717cf0e5bb8d
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2023-11-10 21:52:01 +0700

    feat: init notification

--- apps/website/src/features/Notification/index.tsx
+++ apps/website/src/features/Notification/index.tsx
@@ -44,13 +44,14 @@ export default function Notification(props: Readonly<Props>) {
         <Container>
           <Grid
             container
-            spacing={{ xs: 4, md: 3 }}
+            spacing={{ xs: 4, md: 4 }}
             columns={{ xs: 4, sm: 8, md: 12 }}
+            className="justify-center"
           >
-            <Grid item xs={4} sm={4} md={4}>
+            <Grid item xs={5} sm={5} md={5}>
               <div className="!float-left">
                 <span className="table w-full cursor-pointer ">
-                  <div className="table-cell w-20 align-middle">
+                  <div className="table-cell w-24 align-middle">
                     <img src={trongdong} alt="icon" />
                   </div>
                   <span className="table-cell pl-5 text-lg align-middle text-dark-moderate-blue-800">
@@ -59,10 +60,10 @@ export default function Notification(props: Readonly<Props>) {
                 </span>
               </div>
             </Grid>
-            <Grid item xs={4} sm={4} md={4}>
-              <div className="!float-right">
+            <Grid item xs={5} sm={5} md={5}>
+              <div className="lg:!float-right md:!float-left">
                 <span className="table w-full cursor-pointer ">
-                  <div className="table-cell w-20 align-middle">
+                  <div className="table-cell w-24 align-middle">
                     <img src={trongdong} alt="icon" />
                   </div>
                   <span className="table-cell pl-5 text-lg align-middle text-dark-moderate-blue-800">
