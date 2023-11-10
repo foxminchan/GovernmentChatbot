b869c87ecdfaf3fbd389730fb698040a77b91111
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2023-11-10 22:16:11 +0700

    chore: change name variable

--- apps/website/src/@types/images.ts
+++ apps/website/src/@types/images.ts
@@ -1,21 +1,21 @@
 import dvc_banner from '../assets/images/notification/dvc_banner.jpg';
 import dvc from '../assets/images/notification/dvc.png';
-import thatnghiep from '../assets/images/notification/GQ-Huong-tro cap-that nghiep.png';
-import loidangky from '../assets/images/notification/Loi dang ky tk.png';
+import unemployment from '../assets/images/notification/GQ-Huong-tro cap-that nghiep.png';
+import errorSignUp from '../assets/images/notification/Loi dang ky tk.png';
 import lptb from '../assets/images/notification/lptb.png';
 
 type bannerNotificationType = {
     DVCBanner: string,
     DVC: string,
-    Thatnghiep: string,
-    Loidangky: string,
+    UnEmployment: string,
+    ErrorSignUp: string,
     LPTB: string,
 }
 
 export const bannerNotification: bannerNotificationType = {
     DVCBanner: dvc_banner,
     DVC: dvc,
-    Thatnghiep: thatnghiep,
-    Loidangky: loidangky,
+    UnEmployment: unemployment,
+    ErrorSignUp: errorSignUp,
     LPTB: lptb,
 }
