4d83b3745b49ce9a042bf214acdb14a37b6ad008
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2023-11-10 22:06:00 +0700

    chore: change name variable

--- apps/website/src/@types/images.ts
+++ apps/website/src/@types/images.ts
@@ -1,30 +1,30 @@
 import share from '../assets/images/icons/intro/chiase.svg';
-import danhgia from '../assets/images/icons/intro/danhgia.svg';
-import denghi from '../assets/images/icons/intro/denghi.svg';
+import evaluate from '../assets/images/icons/intro/danhgia.svg';
+import suggest from '../assets/images/icons/intro/denghi.svg';
 import login from '../assets/images/icons/intro/login.svg';
 import payment from '../assets/images/icons/intro/payment.svg';
-import thucchien from '../assets/images/icons/intro/thuchien.svg';
-import timkiem from '../assets/images/icons/intro/timkiem.svg';
-import truyvan from '../assets/images/icons/intro/truyvan.svg';
+import perform from '../assets/images/icons/intro/thuchien.svg';
+import search from '../assets/images/icons/intro/timkiem.svg';
+import query from '../assets/images/icons/intro/truyvan.svg';
 
 type IconIntroType= {
     Share_Icon: string,
-    DanhGia_Icon: string,
-    Denghi_Icon: string,
+    Evaluate_Icon: string,
+    Suggest_Icon: string,
     Login_Icon: string,
     Payment_Icon: string,
-    ThucHien_Icon: string,
-    TimKiem_Icon: string,
-    TruyVan_Icon: string,
+    Perform_Icon: string,
+    Search_Icon: string,
+    Query_Icon: string,
 }
 
 export const IconIntro: IconIntroType ={
     Share_Icon: share,
-    DanhGia_Icon: danhgia,
-    Denghi_Icon: denghi,
+    Evaluate_Icon: evaluate,
+    Suggest_Icon: suggest,
     Login_Icon: login,
     Payment_Icon: payment,
-    ThucHien_Icon: thucchien,
-    TimKiem_Icon: timkiem,
-    TruyVan_Icon: truyvan,
+    Perform_Icon: perform,
+    Search_Icon: search,
+    Query_Icon: query,
 }
