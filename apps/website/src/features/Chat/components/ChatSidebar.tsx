5356145a09b668c49b0ef804a5a69c5b96b53bcf
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2023-11-14 01:35:56 +0700

    feat: init responsive

--- apps/website/src/features/Chat/components/ChatSidebar.tsx
+++ apps/website/src/features/Chat/components/ChatSidebar.tsx
@@ -1,23 +1,17 @@
 import HomeIcon from '@mui/icons-material/Home';
 import ScienceIcon from '@mui/icons-material/Science';
-import logo from '../../../assets/images/logo-rbg.png';
+import logo from '../../../assets/images/caption.svg';
 import TopicButton from './TopicButton';
 
 export default function ChatSidebar() {
   return (
-    // sidebar
-    <div className=" w-[352px] flex-auto bg-white border border-japonica-400 rounded-tr-xl rounded-br-xl">
-      {/* upper sidebar */}
-      <div className="p-10 lg:h-[85%] md:h-[85%] sm: h-[88%]  justify-center align-middle border-japonica-400 border-solid border-b-[1px]">
-        {/* logo */}
+    <div className="hidden lg:flex flex-col md:w-[352px]  bg-white border border-japonica-400 rounded-tr-xl rounded-br-xl">
+      <div className=" lg:h-[85%] md:h-[85%] h-[88%]  justify-center align-middle border-japonica-400 border-solid border-b-[1px] mt-5 ">
         <div className="flex justify-center w-full h-10 mb-10">
-          <img src={logo} alt="logo" className="w-full h-full" />
+          <img src={logo} alt="logo" className="w-[90%] h-full" />
         </div>
-        {/* button */}
-        <TopicButton />
         <TopicButton />
       </div>
-      {/* lower sidebar */}
       <div className="mt-5 ">
         <div className="block px-5 py-0 text-dark-moderate-blue-700">
           <div className="flex mb-5 bg-transparent hover:text-japonica-400">
--- apps/website/src/features/Chat/index.tsx
+++ apps/website/src/features/Chat/index.tsx
@@ -1,10 +1,15 @@
-import { Button } from '@mui/material';
+import clsx from 'clsx';
+import { Button, Container } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
 import SendIcon from '@mui/icons-material/Send';
-import useMetadata from '../../common/hooks/useMetadata';
-import BotChat from './components/BotChat';
-import ChatSidebar from './components/ChatSidebar';
-import GuessChat from './components/GuessChat';
 import StartScreen from './components/StartScreen';
+import ChatSidebar from './components/ChatSidebar';
+import logo from '../../assets/images/caption.svg';
+import TopicButton from './components/TopicButton';
+import { useEffect, useRef, useState } from 'react';
+import ScienceIcon from '@mui/icons-material/Science';
+import useMetadata from '../../common/hooks/useMetadata';
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 
 type Props = {
   title: string;
@@ -12,24 +17,98 @@ type Props = {
 
 export default function Chat(props: Readonly<Props>) {
   useMetadata(props.title);
+
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        !(event.target as Element)?.closest('.toggle-menu-button')
+      ) {
+        setMenuVisible(false);
+      }
+    };
+    document.addEventListener('click', handleOutsideClick);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
   return (
-    <div className="flex h-screen">
-      <ChatSidebar />
-      <div className="flex flex-col items-center bg-white">
-        <StartScreen />
-        <div className="bottom-0 flex flex-col items-center justify-center w-[95%]  mt-auto mb-5  border border-gray-300 rounded-lg">
-          <div className="flex justify-center w-full ml-5 ">
-            <input
-              type="text"
-              placeholder="Nhập câu hỏi của bạn..."
-              className="w-[95%] h-[50px] border-transparent pl-2 outline-none"
-            />
-            <Button className="bg-transparent !text-gray-300 hover:!text-blue-400 hover:bg-transparent ml-5">
-              <SendIcon />
-            </Button>
+    <>
+      <div className="flex h-screen">
+        {menuVisible && (
+          <Container
+            className="absolute top-0 left-0 w-full h-full bg-[#00000033] lg:hidden"
+            onClick={() => {
+              toggleMenu();
+            }}
+          ></Container>
+        )}
+        <ChatSidebar />
+        <div className="flex flex-col items-center w-full bg-white">
+          <div className="absolute top-0 left-0 flex justify-center w-10 h-10 lg:hidden ">
+            <button
+              onClick={toggleMenu}
+              className="flex items-center px-3 py-2 toggle-menu-button hover:text-japonica-400"
+            >
+              <DensityMediumIcon className="w-6 h-6" />
+            </button>
+          </div>
+          <StartScreen />
+          <div className=" flex flex-col items-center justify-center w-[95%]  mt-auto mb-5 border border-gray-300 rounded-lg">
+            <div className="flex justify-center w-full ml-1 lg:ml-5 ">
+              <input
+                type="text"
+                placeholder="Nhập câu hỏi của bạn..."
+                className="w-[95%] h-[50px] border-transparent pl-2 outline-none"
+              />
+              <Button className="bg-transparent !text-gray-300 hover:!text-japonica-400 hover:bg-transparent ml-5">
+                <SendIcon />
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {menuVisible && (
+        <div
+          ref={menuRef}
+          className={clsx(
+            'lg:hidden bg-white-smoke-100 border rounded-tr-lg rounded-br-lg border-japonica-400 top-0 left-0 px-[15px] pt-2 absolute z-10 bg-blend-overlay',
+            menuVisible
+              ? 'h-full w-[260px] transition-all duration-[0.3s]'
+              : 'w-0 transition-all duration-[0.3s]'
+          )}
+        >
+          <div className="lg:h-[85%] md:h[80%] h-[75%]  justify-center align-middle border-japonica-400 border-solid border-b-[1px] mt-10 mb-5">
+            {/* logo */}
+            <div className="flex justify-center w-full h-10 mb-10">
+              <img src={logo} alt="logo" className="w-[90%] h-full" />
+            </div>
+            <TopicButton />
+          </div>
+
+          <div className="mt-5 ">
+            <div className="block px-5 py-0 text-dark-moderate-blue-700">
+              <div className="flex mb-5 bg-transparent hover:text-japonica-400">
+                <HomeIcon className="mr-5" />
+                <span className="text-lg font-medium ">Trang chủ</span>
+              </div>
+              <div className="flex mb-5 bg-transparent hover:text-japonica-400">
+                <ScienceIcon className="mr-5" />
+                <span className="text-lg font-medium ">Thử nghiệm</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
