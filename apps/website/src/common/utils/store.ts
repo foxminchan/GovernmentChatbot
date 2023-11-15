664b39a09daadd2571a6bb8c63d20fbfc5208388
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-11-15 13:09:30 +0700

    feat: add rtk query

--- apps/website/src/common/utils/store.ts
+++ apps/website/src/common/utils/store.ts
@@ -1,11 +1,17 @@
 import thunk from 'redux-thunk';
 import rootReducer from '../../features';
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 const store = configureStore({
   reducer: rootReducer,
   middleware: [thunk],
   devTools: true,
 });
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export { store };
