14dd22328573e6a2f57ebdb95952884521b2b22e
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-11-17 01:03:38 +0700

    fix: clean up type

--- apps/website/src/features/SignUp/types/register.type.ts
+++ apps/website/src/features/SignUp/types/register.type.ts
@@ -1,19 +1,13 @@
-export type RegisterPayload = {
-  user: {
-    name: string;
-    email: string;
-    phone: string;
-    address: string;
-    id_card: string;
-  };
-  password: string;
-};
-
 export type RegisterResponse = {
-  id: string;
+  id?: string;
   name: string;
   email: string;
   phone: string;
   address: string;
   id_card: string;
+};
+
+export type RegisterPayload = {
+  user: Omit<RegisterResponse, 'id'>;
+  password: string;
 };
